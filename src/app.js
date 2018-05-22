e154ddbfa92563bfca0d27723cbf9018c940ccfc
Kondrat1991 <kondrat19911708@gmail.com>
2018-05-22 10:39:59 +0300

    Mick

--- src/app.js
+++ src/app.js
@@ -25,12 +25,6 @@ class App extends Component{
     render(){
         return (
             <Fragment>
-                {/*<Timer time="5" onTimeOut={this.switchTimer} />*/}
-                {/*<button onClick={this.switchTimer}>Переключить таймер</button>*/}
-                {/*<RenderIf condition={this.state.showTimer}>*/}
-                    {/*<Timer time="6000" step="2" autoStart/>*/}
-                {/*</RenderIf>*/}
-                {/*<Timer time='0' onTimeStop={this.showTotalTime} reverse={false} autoStart/>*/}
 
                 <Timer time ={60} onTimeStop={this.showTotalTime} reverse autoStart />
             </Fragment>
